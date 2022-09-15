784888ea9981c5be2d451b804161891377d3ac67
Tushar Miglani <75978513+tusharmiglani2002@users.noreply.github.com>
2022-09-15 17:36:49 +0530

    Changed redirect Url to vercel

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ import "./Login.css";
 //   process.env;
 const REACT_APP_CLIENT_ID = "7ed2b54e50164eeaa4161c555de72240";
 const REACT_APP_AUTHORIZE_URL = "https://accounts.spotify.com/authorize";
-const REACT_APP_REDIRECT_URL = "http://localhost:3000/redirect/";
+const REACT_APP_REDIRECT_URL = "https://spotify-data-search.vercel.app/";
 
 const loginHandler = () => {
   window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
